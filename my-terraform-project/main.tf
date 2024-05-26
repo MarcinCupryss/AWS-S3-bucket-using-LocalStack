d98e7d868d2e0955a05014e06b7951340289f7c0
Marcin Cupryś <162025701+MarcinCuprys13@users.noreply.github.com>
2024-05-26 12:17:56 +0200

    localstack AWS S3 Bucket

---
+++ my-terraform-project/main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+terraform {
+    backend "local" {
+        path = "/.terraform.tfstate"
+    }
+}
